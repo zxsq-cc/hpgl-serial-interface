d7686240574dbda3b8c945636cdc90378151e8a5
zxsq-cc <44452124+zxsq-cc@users.noreply.github.com>
2020-09-11 13:06:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='hpglsender',
-    version='0.1.0rc1'
+    version='0.1.0'
     packages=['hpglsender'],
     include_package_data=True,
     install_requires=[
